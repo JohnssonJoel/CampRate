c8ad91e91f400fcc7947b694058f9a18e8244e43
JohnssonJoel <joel.johnsson@hotmail.se>
2026-04-22 18:51:05 +0200

    feat: add first campground API route

--- server/server.js
+++ server/server.js
@@ -3,8 +3,38 @@ import express from "express";
 const app = express();
 const PORT = 5000;
 
+// Middleware
+app.use(express.json());
+
+// Root route
 app.get("/", (req, res) => {
-    res.send("CampRate backend is running! with nodemon");
+    res.send("CampRate backend is running!");
+});
+
+// API route
+app.get("/api/campgrounds", (req, res) => {
+    const campgrounds = [
+        {
+            id: 1,
+            name: "Åhus Camping",
+            location: "Åhus",
+            rating: 4.5
+        },
+        {
+            id: 2,
+            name: "Böda Sand",
+            location: "Öland",
+            rating: 4.9
+        },
+        {
+            id: 3,
+            name: "Kronocamping Saxnäs",
+            location: "Öland",
+            rating: 4.4
+        }
+    ];
+
+    res.json(campgrounds);
 });
 
 app.listen(PORT, () => {
