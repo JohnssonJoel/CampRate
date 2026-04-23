eff8deb965d03614c8a23f36aade575a32f10a3b
JohnssonJoel <joel.johnsson@hotmail.se>
2026-04-23 15:04:29 +0200

    feat: add GET campground by id route

--- server/controllers/campgroundController.js
+++ server/controllers/campgroundController.js
@@ -24,11 +24,53 @@ export function getAllCampgrounds(req, res) {
 }
 
 export function createCampground(req, res) {
-    const newCampground = req.body;
+    const { id, name, location, rating } = req.body;
+
+    if (!name || !location || !rating === undefined) {
+        return res.status(400).json({
+            error: "name, location and rating are required"
+        });
+    }
+
+    if (typeof rating !== "number") {
+        return res.status(400).json({
+            error: "rating must be a number"
+        });
+    }
+
+    if (rating < 1 || rating > 5) {
+        return res.status(400).json({
+            error: " rating must be between 1 and 5"
+        });
+    }
+
+    const newCampground = {
+        id,
+        name,
+        location,
+        rating
+    };
 
     campgrounds.push(newCampground);
 
-    res.status(201).json(newCampground);
+    res.status(201).json(newCampground)
+}
+
+export function getCampgroundById(req, res) {
+    const id = Number(req.params.id);
+
+    const campground = campgrounds.find(
+        (campground) => campground.id === id
+    );
+
+    if (!campground) {
+        return res.status(404).json({
+            error: "Campground not found"
+        });
+    }
+
+    res.json(campground);
+
 }
 
 
--- server/routes/campgroundRoutes.js
+++ server/routes/campgroundRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { 
     getAllCampgrounds,
-    createCampground
+    createCampground,
+    getCampgroundById
 } from "../controllers/campgroundController.js";
 
 const router = express.Router();
 
 router.get("/", getAllCampgrounds);
+router.get("/:id", getCampgroundById);
 router.post("/", createCampground);
 
 export default router;
